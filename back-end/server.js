6628e8974a61f221843d33dd1bf960de0a801299
Kabil081 <kabilram212@gmail.com>
2025-02-10 16:35:37 +0530

    feat:

--- back-end/server.js
+++ back-end/server.js
@@ -1,19 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
-
 const app = express();
 const PORT = process.env.PORT || 5000;
-
-
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
 .then(() => console.log('MongoDB Connected'))
 .catch(err => console.error('MongoDB Connection Error:', err));
-
-
 app.use(express.json());
 
 app.get('/', (req, res) => {
